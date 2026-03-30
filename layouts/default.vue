8a2e4780320bd4127d978ad58cd3055cc2111e30
Shahriyor856 <shahriyormannonov5@gmail.com>
2026-03-30 12:53:53 +0500

    file tructure is improved

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AppFooter from "~/components/layouts/AppFooter.vue";
+import AppHeader from "~/components/layouts/AppHeader.vue";
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+
+    <main>
+      <slot />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="bg-red-600">Home page</h1>
+  </div>
+</template>
